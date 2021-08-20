6ab8af4a356372618246e5a5e2e2c7f56ab9c562
wenboyu <wenboyu00@gmail.com>
2021-08-21 00:38:17 +0800

    add:leetcode 701

---
+++ leetcode/701.二叉搜索树中的插入操作/701.二叉搜索树中的插入操作.py
@@ -0,0 +1,16 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
+        if root is None:
+            return TreeNode(val=val)
+        if root.val > val:
+            root.left = self.insertIntoBST(root.left, val)
+        if root.val < val:
+            root.right = self.insertIntoBST(root.right, val)
+        return root
