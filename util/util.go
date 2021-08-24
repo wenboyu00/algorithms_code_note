46bd9d7ef7cabb9f4c63e73506ff9bd66fe9603f
wenboyu00 <wenboyu00@gmail.com>
2021-08-24 19:15:51 +0800

    update:util

--- util/util.go
+++ util/util.go
@@ -1,8 +1,26 @@
 package util
 
-func Min(a int, b int) int {
+func IntMin(a int, b int) int {
 	if a < b {
 		return a
 	}
 	return b
+}
+func Intmax(a int, b int) int {
+	if a < b {
+		return b
+	} else {
+		return a
+	}
+}
+
+func IntPow(n, m int) int {
+	if m == 0 {
+		return 1
+	}
+	result := n
+	for i := 2; i <= m; i++ {
+		result *= n
+	}
+	return result
 }
