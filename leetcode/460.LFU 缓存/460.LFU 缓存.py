ebef154b94e0d13654432f69f0f38935badd5843
wenboyu <wenboyu00@gmail.com>
2021-08-30 23:53:31 +0800

    update: leetcode 460

--- leetcode/460.LFU 缓存/460.LFU 缓存.py
+++ leetcode/460.LFU 缓存/460.LFU 缓存.py
@@ -1,7 +1,11 @@
 class Node:
-    def __init__(self, key, val, freq=0):
+    """
+    双向链表节点
+    """
+
+    def __init__(self, key=None, val=None, freq=0):
         self.key = key
-        self.val = val
+        self.value = val
         self.pre = None
         self.next = None
         self.freq = freq
@@ -9,28 +13,26 @@ class Node:
 
 class DLinkedList:
     """
-    双向链表
+    双向链表和一些列所需要的操作
     """
 
     def __init__(self):
-        self.head = Node(0, 0)
-        self.tail = Node(0, 0)
+        self.head = Node()
+        self.tail = Node()
         self.head.next = self.tail
         self.tail.pre = self.head
 
     def insert_first(self, node):
         node.next = self.head.next
+        node.pre = self.head
         self.head.next.pre = node
         self.head.next = node
-        node.pre = self.head
 
     def delete(self, node):
         if self.head.next == self.tail:
             return
         node.pre.next = node.next
         node.next.pre = node.pre
-        node.next = None
-        node.pre = None
 
     def get_last(self):
         if self.head.next == self.tail:
@@ -49,67 +51,20 @@ class LFUCache:
         """
         self.capacity = capacity
         self.min_freq = 0
-        self.freq_map = {}
-        self.key_map = {}
-
-    def _increase(self, node, is_new_node=False):
-        """
-        更新节点访问频率(freq)
-        """
-        if is_new_node:
-            self._minFreq = 1
-            self._set_default_linked_list(node)
-        else:
-            self._delete(node)
-            node.freq += 1
-            self._set_default_linked_list(node)
-            if self.min_freq not in self.freq_map:
-                self.min_freq += 1
-
-    def _set_default_linked_list(self, node):
-        """
-        根据节点的频率，插入对应的LinkedList中，没有则创建
-        """
-        if node.freq not in self.freq_map:
-            self.freq_map[node.freq] = DLinkedList()
-        d_linked_list = self.freq_map[node.freq]
-        d_linked_list.insert_first(node)
-
-    def _delete(self, node):
-        if node.freq not in self.freq_map:
-            return
-        d_linked_list = self.freq_map[node.freq]
-        freq = node.freq
-        d_linked_list.delete(node)
-        if d_linked_list.is_empty():
-            del self.freq_map[freq]
-
-    def _remove_min_freq_node(self):
-        """
-        删除频率最低的元素
-        找到freq_map[min_freq]中最后一个节点删除，同时在key_map也删除
-        如果链表为空，则删除此链表
-        """
-        d_linked_list = self.freq_map[self.min_freq]
-        node = d_linked_list.get_last()
-        d_linked_list.delete(node)
-        del self.key_map[node.key]
-        if d_linked_list.is_empty():
-            del self.freq_map[node.freq]
+        self.freq_map = dict()
+        self.key_map = dict()
 
     def get(self, key: int) -> int:
         """
         获得元素
         - 如果存在 就返回应对val，更新访问元素访问频率
         - 如果不在，返回 -1
-        :param key:
-        :return:
         """
-        if key in self.key_map:
-            node = self.key_map[key]
-            self._increase(node)
-            return node.val
-        return -1
+        if key not in self.key_map:
+            return -1
+        node = self.key_map[key]
+        self._increase(node)
+        return node.value
 
     def put(self, key: int, value: int) -> None:
         """
@@ -130,3 +85,66 @@ class LFUCache:
             node = Node(key, value, 1)
             self._increase(node, True)
             self.key_map[key] = node
+
+    def _increase(self, node, is_new_node=False):
+        """
+        更新节点访问频率(freq)
+        """
+        if is_new_node:
+            self.min_freq = 1
+            self._insert_first(node)
+        else:
+            self._delete(node)
+            node.freq += 1
+            self._insert_first(node)
+            if self.min_freq not in self.freq_map:
+                self.min_freq += 1
+
+    def _insert_first(self, node):
+        """
+        根据节点的频率，插入对应的LinkedList中，没有则创建
+        """
+        if node.freq not in self.freq_map:
+            self.freq_map[node.freq] = DLinkedList()
+        d_linked_list = self.freq_map[node.freq]
+        d_linked_list.insert_first(node)
+
+    def _delete(self, node):
+        if node.freq not in self.freq_map:
+            return
+        d_linked_list = self.freq_map[node.freq]
+        freq = node.freq
+        d_linked_list.delete(node)
+        # 列表为空时，删除freq_map上的索引
+        if d_linked_list.is_empty():
+            del self.freq_map[freq]
+        return node.key
+
+    def _remove_min_freq_node(self):
+        """
+        删除频率最低的元素
+        找到freq_map[min_freq]中最后一个节点删除，同时在key_map也删除
+        如果链表为空，则删除此链表
+        """
+        d_linked_list = self.freq_map[self.min_freq]
+        node = d_linked_list.get_last()
+        d_linked_list.delete(node)
+        del self.key_map[node.key]
+        # 列表为空时，删除freq_map上的索引
+        if d_linked_list.is_empty():
+            del self.freq_map[node.freq]
+
+
+if __name__ == '__main__':
+    lfu = LFUCache(2)
+    print(lfu.put(2, 2))
+    print(lfu.put(1, 1))
+    print(lfu.get(2))
+    print(lfu.get(1))
+    print(lfu.get(2))
+    print(lfu.put(3, 3))
+    print(lfu.put(4, 4))
+    print(lfu.get(3))
+    print(lfu.get(2))
+    print(lfu.get(1))
+    print(lfu.get(4))
