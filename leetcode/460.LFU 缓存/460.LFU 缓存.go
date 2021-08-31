3b6f3fb868a7a19e62061d849e16cc7ef5e09985
wenboyu00 <wenboyu00@gmail.com>
2021-08-31 12:24:40 +0800

    update:leetcode 460

--- leetcode/460.LFU 缓存/460.LFU 缓存.go
+++ leetcode/460.LFU 缓存/460.LFU 缓存.go
@@ -1,1 +1,39 @@
 package main
+
+type Node struct {
+	key, value, freq int
+	pre, next        *Node
+}
+
+func initNode(key, value int) *Node {
+	return &Node{key: key, value: value}
+}
+
+type DLinkedList struct {
+	head, tail *Node
+}
+
+func initDLinkedList() *DLinkedList {
+	DLinked := &DLinkedList{
+		head: initNode(0, 0),
+		tail: initNode(0, 0),
+	}
+	DLinked.head.next = DLinked.tail
+	DLinked.tail.pre = DLinked.head
+	return DLinked
+}
+
+type LFUCache struct {
+}
+
+func Constructor(capacity int) LFUCache {
+
+}
+
+func (this *LFUCache) Get(key int) int {
+
+}
+
+func (this *LFUCache) Put(key int, value int) {
+
+}
