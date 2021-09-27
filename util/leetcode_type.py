8ee9165bd1ccc0939fde1b3d28fbe8daa2968573
wenboyu <wenboyu00@gmail.com>
2021-09-27 21:11:26 +0800

    update: leetcode type

--- util/leetcode_type.py
+++ util/leetcode_type.py
@@ -3,3 +3,9 @@ class ListNode:
         self.val = val
         self.next = next
 
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
