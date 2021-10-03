6f2f101f270f92e78351a1d3be12e90e95cfb00b
wenboyu00 <wenboyu00@gmail.com>
2021-10-03 23:51:10 +0800

    add:leetcode 39

---
+++ leetcode/39.组合总和/39.组合总和.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        result = []
+        path = []
+
+        def backtrack():
+            pass
+
+        return result
+
+
+if __name__ == '__main__':
+    candidates = [2, 3, 6, 7]
+    target = 7
+    print(Solution().combinationSum(candidates, target))
