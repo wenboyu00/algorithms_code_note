5c60d91e9dacd1ab24bff594b5a95e4b231cf401
wenboyu00 <wenboyu00@gmail.com>
2021-10-11 19:00:14 +0800

    add:leetcode 62

---
+++ leetcode/62.不同路径/62.不同路径.py
@@ -0,0 +1,13 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+         """
+         m是行，n是列
+         动态规划
+         base case
+         dp数组
+         动态转移方程
+         """
+         for i in range(m):
+             for j in range(n):
+                 pass
+
