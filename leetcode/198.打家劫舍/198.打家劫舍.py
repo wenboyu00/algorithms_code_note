c018c5da2fc59d01988c89853fa6a2a7639932be
wenboyu00 <wenboyu00@gmail.com>
2021-10-21 18:20:45 +0800

    fix:leetcode 198

--- leetcode/198.打家劫舍/198.打家劫舍.py
+++ leetcode/198.打家劫舍/198.打家劫舍.py
@@ -19,7 +19,7 @@ class Solution:
         dp = [0] * (n + 2)
         # n-1 长度比索引大1，所以从长度 - 1开始
         # -1 是前闭后开，不包括-1，所以索引到0
-        # -1 倒叙，从大到笑
+        # -1 倒叙，从大到小
         for i in range(n - 1, -1, -1):
             dp[i] = max(dp[i + 1], nums[i] + dp[i + 2])
         return dp[0]
