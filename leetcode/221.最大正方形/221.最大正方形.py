ced2fe1459d054b19ed74cb790196e1555acbf53
wenboyu <wenboyu00@gmail.com>
2021-10-25 00:54:38 +0800

    update: leetcode 221

--- leetcode/221.最大正方形/221.最大正方形.py
+++ leetcode/221.最大正方形/221.最大正方形.py
@@ -3,7 +3,24 @@ from typing import List
 
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
-        pass
+        max_side = 0
+        m = len(matrix)
+        n = len(matrix[0])
+        if m == 0 or n == 0:
+            return max_side
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == '1':
+                    # base case，给第一列和第一行dp赋值为1
+                    if i == 0 or j == 1:
+                        dp[i][j] = 1
+                    else:
+                        # 以dp[i][j]为右下角的最长边 = min(上，坐上，左) +1
+                        dp[i][j] = min(dp[i - 1][j], dp[i - 1][j - 1], dp[i][j - 1]) + 1
+                    # 更新最大边长变量
+                    max_side = max(max_side, dp[i][j])
+        return max_side * max_side
 
 
 if __name__ == '__main__':
@@ -11,3 +28,4 @@ if __name__ == '__main__':
               ["1", "0", "1", "1", "1"],
               ["1", "1", "1", "1", "1"],
               ["1", "0", "0", "1", "0"]]
+    print(Solution().maximalSquare(matrix))
