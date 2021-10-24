e80207bcfe6d2a9a3ae13cb0a9c81df94df5a00e
wenboyu <wenboyu00@gmail.com>
2021-10-24 21:20:37 +0800

    add: leetcode 221

---
+++ leetcode/221.最大正方形/221.最大正方形.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        pass
+
+
+if __name__ == '__main__':
+    matrix = [["1", "0", "1", "0", "0"],
+              ["1", "0", "1", "1", "1"],
+              ["1", "1", "1", "1", "1"],
+              ["1", "0", "0", "1", "0"]]
