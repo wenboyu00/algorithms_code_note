888796b87c11aa8ef42440acbf3a5474c15f016a
wenboyu <wenboyu00@gmail.com>
2021-11-06 23:55:23 +0800

    update: leetcode 437

--- leetcode/437.路径总和 III/437.路径总和 III.py
+++ leetcode/437.路径总和 III/437.路径总和 III.py
@@ -2,9 +2,33 @@ from util.leetcode_type import TreeNode
 
 
 class Solution:
+    def __init__(self):
+        self.res = 0
+
     def pathSum(self, root: TreeNode, targetSum: int) -> int:
         """
-        路径和，回溯算法
+        前缀和，回溯算法
+        两个节点之差等于 targetSum 即可
         """
+        # 记录路径中前缀和出现次数
+        # base case 开始是0出现1
+        mapping = {0: 1}
 
+        def dfs(root, cur):
+            # 结束条件
+            if not root:
+                return
+            # 前缀和+当前节点值
+            cur += root.val
+            # 判断是否存在符合条件的前缀和
+            pre = cur - targetSum
+            self.res += mapping.get(pre, 0)
+            # 记录当前前缀和
+            mapping[cur] = mapping.get(cur, 0) + 1
+            dfs(root.left, cur)
+            dfs(root.right, cur)
+            # 回溯，恢复状态
+            mapping[cur] -= 1
 
+        dfs(root, 0)
+        return self.res
