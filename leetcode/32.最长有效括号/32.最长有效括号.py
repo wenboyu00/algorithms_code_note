a902ec5a8aa701fa5de0b1bcf60879d78169c616
wenboyu <wenboyu00@gmail.com>
2021-11-19 23:35:08 +0800

    add: leetcode 32

---
+++ leetcode/32.最长有效括号/32.最长有效括号.py
@@ -0,0 +1,13 @@
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        """
+        动态规划
+        dp[i]:每个位置上合法括号的数量
+
+        """
+        pass
+
+
+if __name__ == '__main__':
+    s = "(()"
+    print(Solution().longestValidParentheses(s))
