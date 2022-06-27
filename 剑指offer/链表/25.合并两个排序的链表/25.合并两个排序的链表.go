74c75ba1ac8e7f71d276b452085164c56c13ce61
wenboyu <wenboyu00@gmail.com>
2022-06-28 00:11:00 +0800

    update: offer 25

---
+++ 剑指offer/链表/25.合并两个排序的链表/25.合并两个排序的链表.go
@@ -0,0 +1,27 @@
+package 链表
+
+import "algorithms-code-note/剑指offer/structures"
+
+type ListNode = structures.ListNode
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	dummy := &ListNode{Val: -1}
+	tail := dummy
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			tail.Next = l1
+			l1 = l1.Next
+		} else {
+			tail.Next = l2
+			l2 = l2.Next
+		}
+		tail = tail.Next
+	}
+	if l1 != nil {
+		tail.Next = l1
+	}
+	if l2 != nil {
+		tail.Next = l2
+	}
+	return dummy.Next
+}
